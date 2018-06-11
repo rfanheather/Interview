b032ff88220e3a069317e402ac4f49715e7f64a2
Rong <rongfanheather@gmail.com>
2018-06-11 18:07:33 -0400

    Update KMPSubstringSearch.java

--- KMPSubstringSearch.java
+++ KMPSubstringSearch.java
@@ -1,6 +1,6 @@
 /**
  * Date 06/11/2018
- * @author Rong
+ * @author Heather Fan
  * 
  * Do pattern matching using KMP algorithm
  * 
@@ -10,9 +10,9 @@
  * https://www.youtube.com/watch?v=GTJr8OvyEVQ
  */
 
-public class KMPSubstringSearch {
+public class Solution {
     public int[] hasSubstring(char[] text, char[] pattern) {
-        Lint[] rst = new int[2];
+        int[] rst = new int[2];
         if (text == null || text.length == 0 || pattern == null || pattern.length == 0) {
             return rst;
         }
@@ -23,6 +23,75 @@ public class KMPSubstringSearch {
     }
     
     private int[] buildTempArray(char[] pattern) {
+        int j = 0;
+        int i = 0;
+        int len = pattern.length;
+        int[] temp = new int[len];
+        temp[0] = 0;
         
+        for(i = 1; i < len; i++) {
+            char c = pattern[i];
+            if (c == pattern[j]) {
+                temp[i] = j + 1;
+                j++;
+                continue;
+            } 
+            
+            while (j != 0) {
+                j = temp[j - 1];
+                if (c == pattern[j]) {
+                    temp[i] = j + 1;
+                    j++;
+                    break;
+                }
+            }
+            
+            if (j == 0) {
+                temp[i] = 0;
+            }
+        } // end of for
+        
+        return temp;
+    }
+    
+    private int[] KMPsearch(char[] text, char[] pattern, int[] temp) {
+        int[] rst = new int[2];
+        int i = 0;
+        int j = 0; // index in pattern
+        int pLen = pattern.length;
+        int tLen = text.length;
+        for (i = 0; i < tLen; i++) {
+            if (j == pLen) {
+                rst[0] = i - pLen;
+                rst[1] = i - 1;
+                return rst;
+            }
+            
+            if (text[i] != pattern[j]) {
+            	    if (j != 0) {
+            	    	    j = temp[j - 1];
+            	    	    	i--;
+            	    }
+            } else {
+            	    j++;
+            }
+        }
+        
+        if (j == pLen) {
+          	rst[0] = tLen - pLen;
+            rst[1] = tLen - 1;
+        }
+        
+        return rst;
+    }
+    
+    public static void main(String[] args) {
+        Solution kmp = new Solution();
+        String text = "abcxabcdabcdabcy";
+        String pattern = "abcdabcy";
+        int[] rst = kmp.hasSubstring(text.toCharArray(), pattern.toCharArray());
+        for (int i : rst) {
+        	    System.out.println(i);
+        }
     }
 }
