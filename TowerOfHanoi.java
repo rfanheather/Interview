e9291c7ec23f84e6bf9cc0f895b607d06c78b6c0
Rong <rongfanheather@gmail.com>
2018-06-16 16:36:53 -0400

    Update TowerOfHanoi.java

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -1,1 +1,37 @@
-
+public class TowerOfHanoi{
+    Stack<Integer> disks;
+    
+    public TowerOfHanoi(){
+        disks = new Stack<>();
+    }
+    
+    private boolean add(int d) {
+        if (!disks.isEmpty() && disks.peek() <= d) {
+            return false;
+        } else {
+            disks.push(d);
+            return true;
+        }
+    }
+    
+    private void moveTopTo(TowerOfHanoi t) {
+        int d = disks.peek();
+        if (t.add(d)){
+            disks.pop();
+        }
+    }
+    
+    public Stack<Integer> getDisks() {
+        return disks;
+    }
+    
+    public void moveToDestination(int n, TowerOfHanoi destination, TowerOfHanoi buffer) {
+        if (n <= 0) {
+            return;
+        }
+        
+        moveToDestination(n - 1, buffer, destination);
+        moveTopTo(destination);
+        buffer.moveToDestination(n - 1, destination, this);
+    }
+}
